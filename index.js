f7336c063bd4dbb9de94469570540337f9d64638
tetete2016 <tetetelego2013@gmail.com>
2016-05-08 12:30:43 +0900

    added new game

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('/CircleBall/CircleBall.html'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     //response.render('pages/index');
-    response.send("hello this is test");
+    response.send("error");
 });
 
 app.listen(app.get('port'), function() {
