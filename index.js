2a4c348e9a7388198bfa9a4f311a718d2517796d
tetete2016 <tetetelego2013@gmail.com>
2016-05-08 12:42:31 +0900

    changed path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
-app.use(express.static('CircleBall'));
+app.use(express.static('public'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
